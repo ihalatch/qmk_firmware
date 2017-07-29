cdfc3aff2a6464e65f7a1584a24f3779b27ca11f
Ethan Madden <emadden@isilon.com>
2017-07-28 17:22:54 -0700

    Started fixing the matrix

--- keyboards/ps2avrGB/ps2avrGB.h
+++ keyboards/ps2avrGB/ps2avrGB.h
@@ -41,11 +41,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define KC_KEYMAP( \
     K05, K25, K35, K45, K55, K06, KA6, KA7, K07, KB5, KC5, KD5, KE5, KD1, KE1, KE2, \
-    K04, K14, K24, K34, K44, K54, K16, KB6, KB7, K17, KA4, KB4, KC4, KE4,      KD0, \
-    K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC3, KD3,      K67, \
-    K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, KB2, KD2,           KE0, \
-    K01, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1,           K86, K77, \
-    K00, K10, K20,           K56,                     K57, KB0, KC0, K66, K76, K96  \
+    K16, K17, K27, K44, KE6, K46, K56, KB6, K67, K77, KD2, K86, K66, KA1,      KD0, \
+    K11, K13, K23, K33, K43, K41, K51, KC6, KC7, K02, KE7, KB3, KC3, KA2,      KB7, \
+    K21, K12, K22, K32, K42, KD6, K36, K52, KD7, K31, KD3, KB2, KA4,           KE0, \
+    K01, K14, K24, K34, K47, K26, K04, K54, K37, K53, KB1, KE4,           KB4, K03, \
+    K00, K10, K20,           KA3,                     K57, KB0, KC0, KC4, K76, K96  \
 ) \
 { \
     { KC_##K00, KC_##K10, KC_##K20, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##KB0, KC_##KC0, KC_##KD0, KC_##KE0 }, \
