abf991774386a058366df5626fda4299c958aff2
Ethan Madden <emadden@isilon.com>
2017-09-08 15:14:55 -0700

    Everything works except the rightmost column

--- keyboards/ps2avrGB/config.h
+++ keyboards/ps2avrGB/config.h
@@ -24,15 +24,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x422D
 // TODO: share these strings with usbconfig.h
 // Edit usbconfig.h to change these.
-#define MANUFACTURER    winkeyless.kr
+#define MANUFACTURER    NotActuallyWinkeyless
 #define PRODUCT         ps2avrGB
 
-/* matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 17
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 10
 
-#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4, B5 }
-#define MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, C7, C6, C5, C4, C3, C2, C1, C0, D7}
+#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4, B5, B6, B7 }
+#define MATRIX_COL_PINS { D7, A1, A2, A3, A4, A5, A6, C7, C6, C5}
 #define UNUSED_PINS
 
 #define DIODE_DIRECTION COL2ROW
--- keyboards/ps2avrGB/ps2avrGB.h
+++ keyboards/ps2avrGB/ps2avrGB.h
@@ -21,17 +21,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 #define KEYMAP( \
-  K00, K10, K20, K13, K17, K1B, K1C, K30, K0D, K40, K2D, K0B, K0C, K1A, K0E,\
-   K11, K01, K02, K03, K04, K14, K15, K05, K06, K07, K08, K18, K16,  K2A,   \
-    K12, K21, K22, K23, K24, K34, K35, K25, K26, K27, K28, K38,   K2B,      \
-    K19,   K41, K42, K33, K3B, K1D, K36, K45, K4A, K4B, K37,    K4D,    K3D,\
-  K09,   K0A,   K39,           K3A,                 K31,   K32,   K4C,   K29\
+  K61, K71, K72, K73, K74, K64, K65, K75, K76, K77, K78, K68, K66, K00, K10,\
+   K11, K01, K02, K03, K04, K14, K15, K05, K06, K07, K08, K18, K16,  K20,   \
+    K12, K21, K22, K23, K24, K34, K35, K25, K26, K27, K28, K38,   K30,      \
+  K19, K13, K41, K42, K43, K44, K54, K55, K45, K46, K47, K58,   K49,    K40,\
+  K09,   K60,   K39,           K50,                 K59,   K69,   K37,   K29\
 ){ \
-  { K00, K01, K02, K03,   K04,   K05, K06, K07, K08, K09, K0A,   K0B,   K0C,   K0D, K0E}, \
-  { K10, K11, K12, K13,   K14,   K15, K16, K17, K18, K19, K1A,   K1B,   K1C,   K1D, KC_NO}, \
-  { K20, K21, K22, K23,   K24,   K25, K26, K27, K28, K29, K2A,   K2B,   KC_NO, K2D, KC_NO}, \
-  { K30, K31, K32, K33,   K34,   K35, K36, K37, K38, K39, K3A,   K3B,   KC_NO, K3D, KC_NO}, \
-  { K40, K41, K42, KC_NO, KC_NO, K45, K4A, K4B, K4C, K4D, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO}, \
+  { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09}, \
+  { K10, K11, K12, K13, K14, K15, K16, KC_NO, K18, K19}, \
+  { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29}, \
+  { K30, KC_NO, KC_NO, KC_NO, K34, K35, KC_NO, K37, K38, K39}, \
+  { K40, K41, K42, K43, K44, K45, K46, K47, KC_NO, K49}, \
+  { K50, KC_NO, KC_NO, KC_NO, K54, K55, KC_NO, KC_NO, K58, K59}, \
+  { K60, K61, KC_NO, KC_NO, K64, K65, K66, KC_NO, K68, K69}, \
+  { KC_NO, K71, K72, K73, K74, K75, K76, K77, K78, KC_NO}, \
 }
 
 #endif
